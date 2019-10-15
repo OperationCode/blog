9aa40ba8e2d25fbd1a511125bd19ce5eeef28fcc
Kyle Holmberg <kholmberg@acorns.com>
2019-10-14 18:13:18 -0700

    Remove deleted import

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import Layout from '../components/Layout';
 import Landing from '../sections/Landing';
 import About from '../sections/About';
 import Projects from '../sections/Projects';
-import Writing from '../sections/Writing';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
@@ -13,7 +12,6 @@ const IndexPage = () => (
     <Landing />
     <About />
     <Projects />
-    <Writing />
     <Footer />
   </Layout>
 );
