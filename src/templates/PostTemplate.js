d51006e7a07594c1897bfbb389954218078c3b94
kelly <ksmacleod99@gmail.com>
2019-10-24 20:31:38 -0400

    starts template for post pages and creates gatsby-node.js

---
+++ src/templates/PostTemplate.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import Helmet from 'react-helmet';
+import Layout from '../components/Layout';
+
+class BlogPostTemplate extends React.Component {
+  render() {
+    const post = get(this.props, 'data.contentfulBlogPost');
+    const siteTitle = get(this.props, 'data.site.siteMetadata.title');
+
+    return (
+      <Layout location={this.props.location}>
+        <div style={{ background: '#fff' }}>
+          <Helmet title={`${post.title} | ${siteTitle}`} />
+          <div className="wrapper">
+            <h1 className="section-headline">{post.title}</h1>
+            <p
+              style={{
+                display: 'block',
+              }}
+            >
+              {post.publishDate}
+            </p>
+            <div
+              dangerouslySetInnerHTML={{
+                __html: post.body.childMarkdownRemark.html,
+              }}
+            />
+          </div>
+        </div>
+      </Layout>
+    );
+  }
+}
+
+export default BlogPostTemplate;
+
+export const pageQuery = graphql`
+  query BlogPostBySlug($slug: String!) {
+    contentfulBlogPost(slug: { eq: $slug }) {
+      title
+      publishDate(formatString: "MMMM Do, YYYY")
+      heroImage {
+        fluid(maxWidth: 1180, background: "rgb:000000") {
+          ...GatsbyContentfulFluid_tracedSVG
+        }
+      }
+      body {
+        childMarkdownRemark {
+          html
+        }
+      }
+    }
+  }
+`;
